df2f6f3dd9d3c41d3d06c0b0145db25b913e5573
KateVu <33895784+KateVu@users.noreply.github.com>
2019-08-21 12:29:17 +1000

    Add files via upload
    re

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Quyen Vu Thi Tu <SID102418320>
+ * version 0.1 last updated day 
+ */
+
+public class Car {
+	protected String regisNumber;
+	protected String owner;
+	protected boolean isStaffMember;
+	
+	public Car(String iniRegisNumber, String iniOwner, boolean iniIsStaffMember ) {
+		regisNumber = iniRegisNumber;
+		owner = iniOwner;
+		isStaffMember = iniIsStaffMember;
+	}
+	
+}
