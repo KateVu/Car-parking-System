e2188ad2c68886ee9ed0e2e12d52db4aaaeb6acf
KateVu <33895784+KateVu@users.noreply.github.com>
2019-08-21 15:19:39 +1000

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -4,15 +4,38 @@
  * version 0.1 last updated day 
  */
 
+
 public class Car {
-	protected String regisNumber;
-	protected String owner;
-	protected boolean isStaffMember;
+	private String regisNumber;//registration number of the car
+	private String owner; //Name of the owner of the car
+	private boolean isStaffMember;//give true if car belonged to a staff member
 	
-	public Car(String iniRegisNumber, String iniOwner, boolean iniIsStaffMember ) {
-		regisNumber = iniRegisNumber;
-		owner = iniOwner;
-		isStaffMember = iniIsStaffMember;
+	//Constructor with registration number, name of owner and know whether owner is staffmember
+	public Car(String iniRegisNumber, String iniOwner, boolean iniIsStaffMember) {
+		this.regisNumber = iniRegisNumber;
+		this.owner = iniOwner;
+		this.isStaffMember = iniIsStaffMember;
 	}
 	
+	//get onwer name of the car
+	public String getOwner () {
+		return this.owner;
+	}
+	
+	//get registration number of the car
+	public String getRegisNumber() {
+		return this.regisNumber;
+	}
+	
+	public boolean getIsStaffMember() {
+		return this.isStaffMember;
+	}
+	
+	public String toString() {
+		if (this.isStaffMember) {
+			return ("Owner: " + this.owner + "; Registration number: " + this.regisNumber + "; Staff member");
+		} else {
+			return ("Owner: " + this.owner + "; Registration number: " + this.regisNumber + "; Not staff member");
+		}
+	}
 }
