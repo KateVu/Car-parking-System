d55f6cd11d2d4db3757abbb8e1c37d4a4cea77de
KateVu <33895784+KateVu@users.noreply.github.com>
2019-08-21 15:20:56 +1000

    Update CarPark.java

--- src/CarPark.java
+++ src/CarPark.java
@@ -1,4 +1,26 @@
+/**
+ * CarPark object to maintain a list of parking slots
+ * @author Quyen Vu Thi Tu SID 102418320
+ * version 0.1
+ */
+
+import java.util.*;
+
 
 public class CarPark {
-
+	protected ArrayList<ParkingSlot> list = new ArrayList<ParkingSlot>();//list of parking slot
+	
+	/**
+	 * get a parking slot by its identifier
+	 * @param slotID
+	 * @return
+	 */
+	public ParkingSlot getParkSlot (String slotID) {
+		int size = this.list.size();
+		for (int i = 0; i < size ; i++) {
+			if (this.list.get(i).getSlotId() == slotID) {
+				return this.list.get(i);
+			}
+		}
+	}
 }
